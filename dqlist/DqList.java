2b04009a28d4e53d66f3336c5a8d9b4d2bf2708e
ika3x <ika3ggalt1@gmail.com>
2026-05-28 22:22:00 +0900

    Add DqLists

---
+++ dqlist/DqList.java
@@ -0,0 +1,206 @@
+class DqList {
+    private static final int DEFAULT_CAPACITY = 8;
+
+    public static class IntDqList {
+        private int[] dat;
+        private int head;
+        private int tail;
+        private int mask;
+        private int size;
+
+        public IntDqList() {
+            this(DEFAULT_CAPACITY);
+        }
+
+        public IntDqList(int capacity) {
+            int n = 1;
+            while (n < capacity) {n <<= 1;}
+            dat = new int[n];
+            mask = n - 1;
+        }
+
+        public void addLast(int x) {
+            if (size == dat.length) grow();
+            dat[tail] = x;
+            tail = (tail + 1) & mask;
+            size++;
+        }
+
+        public void addFirst(int x) {
+            if (size == dat.length) grow();
+            head = (head - 1) & mask;
+            dat[head] = x;
+            size++;
+        }
+
+        public int pollFirst() {
+            if (size == 0) throw new NoSuchElementException();
+            int res = dat[head];
+            head = (head + 1) & mask;
+            size--;
+            return res;
+        }
+
+        public int pollLast() {
+            if (size == 0) throw new NoSuchElementException();
+            tail = (tail - 1) & mask;
+            int res = dat[tail];
+            size--;
+            return res;
+        }
+
+        public int get(int index) {
+            if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+            return dat[(head + index) & mask];
+        }
+
+        public int getFirst() {
+            if (size == 0) throw new NoSuchElementException();
+            return dat[head];
+        }
+
+        public int getLast() {
+            if (size == 0) throw new NoSuchElementException();
+            return dat[(tail - 1) & mask];
+        }
+
+        private void grow() {
+            int oldCap = dat.length;
+            int[] newDat = new int[oldCap << 1];
+            int len1 = oldCap - head;
+            System.arraycopy(dat, head, newDat, 0, len1);
+            System.arraycopy(dat, 0, newDat, len1, head);
+            
+            dat = newDat;
+            head = 0;
+            tail = oldCap;
+            mask = dat.length - 1;
+        }
+
+        public int size() { return size; }
+        public boolean isEmpty() { return size == 0; }
+    }
+
+    public static class LongDqList {
+        private long[] dat;
+        private int head;
+        private int tail;
+        private int mask;
+        private int size;
+
+        public LongDqList() {
+            this(DEFAULT_CAPACITY);
+        }
+
+        public LongDqList(int capacity) {
+            int n = 1;
+            while (n < capacity) {n <<= 1;}
+            dat = new long[n];
+            mask = n - 1;
+        }
+
+        public void addLast(long x) {
+            if (size == dat.length) grow();
+            dat[tail] = x;
+            tail = (tail + 1) & mask;
+            size++;
+        }
+
+        public void addFirst(long x) {
+            if (size == dat.length) grow();
+            head = (head - 1) & mask;
+            dat[head] = x;
+            size++;
+        }
+
+        public long get(int index) {
+            if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+            return dat[(head + index) & mask];
+        }
+
+        public long getFirst() {
+            if (size == 0) throw new NoSuchElementException();
+            return dat[head];
+        }
+
+        public long getLast() {
+            if (size == 0) throw new NoSuchElementException();
+            return dat[(tail - 1) & mask];
+        }
+
+        public long pollFirst() {
+            if (size == 0) throw new NoSuchElementException();
+            long res = dat[head];
+            head = (head + 1) & mask;
+            size--;
+            return res;
+        }
+
+        public long pollLast() {
+            if (size == 0) throw new NoSuchElementException();
+            tail = (tail - 1) & mask;
+            long res = dat[tail];
+            size--;
+            return res;
+        }
+
+        private void grow() {
+            int oldCap = dat.length;
+            long[] newDat = new long[oldCap << 1];
+            int len1 = oldCap - head;
+            System.arraycopy(dat, head, newDat, 0, len1);
+            System.arraycopy(dat, 0, newDat, len1, head);
+            
+            dat = newDat;
+            head = 0;
+            tail = oldCap;
+            mask = dat.length - 1;
+        }
+
+        public int size() { return size; }
+        public boolean isEmpty() { return size == 0; }
+    }
+
+    public static class DoubleDqList {
+        private final LongDqList dql;
+        
+        public DoubleDqList(int capacity) {
+            dql = new LongDqList(capacity);
+        }
+
+        public DoubleDqList() {
+            this(DEFAULT_CAPACITY);
+        }
+
+        public void addLast(double x) {
+            dql.addLast(Double.doubleToRawLongBits(x));
+        }
+
+        public void addFirst(double x) {
+            dql.addFirst(Double.doubleToRawLongBits(x));
+        }
+
+        public double get(int index) {
+            return Double.longBitsToDouble(dql.get(index));
+        }
+
+        public double getFirst() {
+            return Double.longBitsToDouble(dql.getFirst());
+        }
+
+        public double getLast() {
+            return Double.longBitsToDouble(dql.getLast());
+        }
+
+        public double pollFirst() {
+            return Double.longBitsToDouble(dql.pollFirst());
+        }
+
+        public double pollLast() {
+            return Double.longBitsToDouble(dql.pollLast());
+        }
+
+        public int size() { return dql.size(); }
+        public boolean isEmpty() { return dql.isEmpty(); }
+    }
+}
---
+++ dqlist/src/IntDqList.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class IntDqList {
+
+    // ignore this
+    private static final int DEFAULT_CAPACITY = 8;
+    // endIgnore
+
+    private int[] dat;
+    private int head;
+    private int tail;
+    private int mask;
+    private int size;
+
+    public IntDqList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public IntDqList(int capacity) {
+        int n = 1;
+        while (n < capacity) {n <<= 1;}
+        dat = new int[n];
+        mask = n - 1;
+    }
+
+    public void addLast(int x) {
+        if (size == dat.length) grow();
+        dat[tail] = x;
+        tail = (tail + 1) & mask;
+        size++;
+    }
+
+    public void addFirst(int x) {
+        if (size == dat.length) grow();
+        head = (head - 1) & mask;
+        dat[head] = x;
+        size++;
+    }
+
+    public int pollFirst() {
+        if (size == 0) throw new NoSuchElementException();
+        int res = dat[head];
+        head = (head + 1) & mask;
+        size--;
+        return res;
+    }
+
+    public int pollLast() {
+        if (size == 0) throw new NoSuchElementException();
+        tail = (tail - 1) & mask;
+        int res = dat[tail];
+        size--;
+        return res;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return dat[(head + index) & mask];
+    }
+
+    public int getFirst() {
+        if (size == 0) throw new NoSuchElementException();
+        return dat[head];
+    }
+
+    public int getLast() {
+        if (size == 0) throw new NoSuchElementException();
+        return dat[(tail - 1) & mask];
+    }
+
+    private void grow() {
+        int oldCap = dat.length;
+        int[] newDat = new int[oldCap << 1];
+        int len1 = oldCap - head;
+        System.arraycopy(dat, head, newDat, 0, len1);
+        System.arraycopy(dat, 0, newDat, len1, head);
+        
+        dat = newDat;
+        head = 0;
+        tail = oldCap;
+        mask = dat.length - 1;
+    }
+
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
+}
